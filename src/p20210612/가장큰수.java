57d1b10180b7567d3ec9a1266a8a303ff59562bc
Yujaehyeong <mynameisyjh@gmail.com>
2021-06-12 12:05:31 +0900

    프로그래머스 가장 큰 수 문제풀이

---
+++ src/p20210612/가장큰수.java
@@ -0,0 +1,28 @@
+package p20210612;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class 가장큰수 {
+    public String solution(int[] numbers) {
+
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            list.add(numbers[i]+"");
+        }
+
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o2 + o1).compareTo(o1 + o2);
+            }
+        });
+
+        String answer = String.join("", list);
+
+        return answer.startsWith("00") ? "0" : answer;
+    }
+}
